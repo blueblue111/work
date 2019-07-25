172ae0a4fc621f93fdcf5539f8add68be0ca6fa4
limengya <limengya@awinic.com.cn>
2019-07-25 11:38:22 +0800

    commit value

---
+++ value.py
@@ -0,0 +1,170 @@
+# counter = 0
+# miles = 1000.0
+# name = "Bob"
+# kilometers = 1.609 * miles
+# n = 10
+# n = n * 10
+# n += 1
+# print(counter,miles,name,kilometers,n)
+
+# a = 100
+# print(a)
+# a = "hehe"
+# print(a)
+
+# name = input('Enter name:')
+# print(name)
+
+# num = input('Enter num:')
+# print(int(num ) + 1) 
+
+# a = 1 
+# b = 2
+# print(a/b)
+# a = 1.0
+# b = 2
+# print(a/b)
+
+# print('I\'m \"OK\"!')
+# print('I\'m learning\nPython')
+# print('\\\n\\')
+
+# print('\\\t\\')
+# print(r'\\\t\\')
+
+# print('''linel
+# line2
+# line3''')
+
+# print(r'''hello,\n
+# world''')
+
+# a = 'ABC'
+# b = a
+# a = 'XYZ'
+# print(b)
+
+# n = 123
+# f = 456.789
+# s1 = 'Hello, world'
+# s2 = 'Hello, \'Adam\''
+# s3 = r'Hello, "Bart"'
+# s4 = r'''Hello,
+# Lisa！'''
+# print(n,f,s1,s2,s3,s4)
+
+#!/user/bin/env python3
+#-*- coding:utf-8 -*-
+
+# print ('中文测试正常')
+
+# print('%2d-%02d' %(3,1))
+# print('%.2f' % 3.1415926)
+
+# s1 = 72
+# s2 = 85
+# r = (s2 - s1)/s1*100
+# print( '%.1f%%'%r)
+# print('{0:.1f}%'.format(r))
+
+# print('%.1f%%'%r)
+# print('{0:.1f}%'.format(r))
+
+# classmates = ['Michael','Bob','Tracy']
+# print(classmates)
+# print(len(classmates))
+
+# L = [
+    # ['Apple','Google','Microsoft'],
+	# ['Java','Python','Ruby','PHP'],
+	# ['Adam','Bart','Lisa']
+# ]
+# print(L[0][0])
+# print(L[1][1])
+# print(L[2][2])
+
+# age = 20
+# if age >= 18:
+    # print('your age is', age)
+    # print('adult')
+
+# age = 3
+# if age >= 18:
+    # print('your age is', age)
+    # print('adult')
+# else:
+    # print('your age is', age)
+    # print('teenager')
+	
+# age = 3
+# if age >= 18:
+    # print('adult')
+# elif age >= 6:
+    # print('teenager')
+# else:
+    # print('kid')
+	
+# height = 1.75
+# weight = 80.5
+# BMI = weight / (height**2)
+# print(BMI)
+# if BMI >= 32:
+    # print('严重肥胖')
+# elif BMI >= 28:
+    # print('肥胖')
+# elif BMI >= 25:
+    # print('过重')
+# elif BMI >= 18.5:
+    # print('正常')
+# else:
+    # print('过轻')
+	
+# names = ['Michael','Bob','Tracy']
+# for name in names:
+    # print(name)	
+	
+# sum = 0
+# for x in [1,2,3,4,5,6,7,8,9,10]:
+    # sum = sum + x
+# print(sum)	
+	
+# sum = 0
+# for x in range():
+    # sum = sum + x
+# print(sum)	
+		
+# L = ['Bart','Lisa','Adam']
+# for name in L:
+    # print(name)	
+# x = len(L) - 1
+# while x >= 0:
+    # print('Hello,%s!'% L[x])
+    # x = x - 1
+
+# n = 1
+# while n <= 100:
+    # if n > 10:
+	    # break
+    # print(n)
+    # n = n + 1
+# print('END')	
+	
+# n = 0
+# while n < 10:
+    # n = n + 1
+    # if n % 2 == 0:
+	    # continue
+    # print(n)
+	
+# n = 0
+# while n >= 0:
+    # n = n + 1
+    # print(n)
+	
+
+	
+	
+
+
+
+
